dd5005b3257f830a58037bf1ef1f0ed2477d03ac
Toshiaki Maki <makingx@gmail.com>
2023-09-08 10:00:54 +0900

    Make sure it works with Spring Session

--- src/main/java/com/example/account/Account.java
+++ src/main/java/com/example/account/Account.java
@@ -1,6 +1,9 @@
 package com.example.account;
 
-public record Account(String username, String password, String twoFactorSecret, boolean twoFactorEnabled) {
+import java.io.Serializable;
+
+public record Account(String username, String password, String twoFactorSecret,
+		boolean twoFactorEnabled) implements Serializable {
 
 	public Account withPassword(String password) {
 		return new Account(this.username, password, this.twoFactorSecret, this.twoFactorEnabled);
